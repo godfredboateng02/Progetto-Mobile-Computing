e8823ba56362617d743868b9c12bfc6e2e83b70d
Godfred <godfred.boateng@studenti.unimi.it>
2025-02-06 13:57:42 +0100

    inserimento model

---
+++ provaP1/my-app/model/CommunicationController.js
@@ -0,0 +1,97 @@
+export default class CommunicationController {
+    static BASE_URL = "https://develop.ewlab.di.unimi.it/mc/2425/";
+    static sid = "3UjfJEE2ukhoqWBqw0mbwVeMJXFy6XIVsgj6bXCOkQsMvyfl87vC4PLeXRMzYqNX"; //TODO: find a way to set the sid
+    
+  
+    static async genericRequest(endpoint, verb, queryParams, bodyParams) {
+      console.log("genericRequest called");
+  
+      const queryParamsFormatted = new URLSearchParams(queryParams).toString();
+      const url = this.BASE_URL + endpoint + "?" + queryParamsFormatted;
+  
+      console.log("Sending " + verb + " request to: " + url);
+  
+      let fetchData = {
+        method: verb,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      };
+
+      if (verb !== "GET") {
+        fetchData.body = JSON.stringify(bodyParams);
+      }
+  
+      let httpResponse;
+      try {
+        httpResponse = await fetch(url, fetchData);
+      } catch (error) {
+        // This means that the fetch request failed.
+        // The request could not be sent or the server did not respond.
+        console.error("Error during fetch request: ", error);
+        throw error;
+      }
+  
+      const status = httpResponse.status;
+      console.log("HTTP response status: ", status);
+      if (status === 200) {
+        // 200 means that the request was successful.
+        // The server responded with the data requested in JSON format.
+        let deserializedObject = await httpResponse.json();
+        return deserializedObject;
+      } else if (status === 204) {
+        // 204 means that the server has successfully processed the request
+        // but that there is no content to send back.
+        return null;
+      } else {
+        // The server responded with an error status.
+        const errorObject = await httpResponse.json();
+        console.error("Error message from the server:", errorObject);
+        throw errorObject;
+      }
+    }
+  
+    static async genericGetRequest(endpoint, queryParams) {
+      console.log("genericGetRequest called");
+      return await this.genericRequest(endpoint, "GET", queryParams, {});
+    }
+  
+    static async getUserInfo(uid){
+        let endpoint = "user/" + uid;
+        let queryParams = { sid: this.sid };
+        console.log("getUserInfo called with endpoint: ",endpoint, " and queryParams: ", queryParams);
+        return await this.genericGetRequest(endpoint, queryParams);
+    }
+
+    static async putUserInfo(uid, bodyParams){
+        let endpoint = "user/" + uid;
+        let queryParams = {sid: this.sid, firstName: bodyParams.firstName, lastName: bodyParams.lastName, cardFullName: bodyParams.cardFullName, cardNumber: bodyParams.cardNumber, cardExpireMonth: bodyParams.cardExpireMonth, cardExpireYear: bodyParams.cardExpireYear, cardCVV: bodyParams.cardCVV, orderStatus: bodyParams.orderStatus};
+        console.log("putUserInfo called with endpoint: ",endpoint, " and queryParams: ", queryParams);
+        return await this.genericRequest(endpoint, "PUT", queryParams, bodyParams);
+    }
+
+    static async getMenus(){
+        let endpoint = "menu";
+        let queryParams = { sid: this.sid, lat: 45.4642, lng: 9.1900 };
+        console.log("getMenus called with endpoint: ",endpoint, " and queryParams: ", queryParams);
+        return await this.genericGetRequest(endpoint, queryParams);
+    }
+
+    static async getMenuImage(mid){
+        let endpoint = "menu/" + mid + "/image";
+        let queryParams = { sid: this.sid, mid: mid };
+        console.log("getMenuImage called with endpoint: ",endpoint, " and queryParams: ", queryParams);
+        return await this.genericGetRequest(endpoint, queryParams);
+    }
+
+    static async getMenuDetails(mid){
+      let endpoint = "menu/"+mid
+      let queryParams = {sid: this.sid, lat: 45.4642, lng: 9.1900, mid: this.mid }
+      console.log("getMenuDetails called with endpoint: ",endpoint," and queryParams: ",queryParams);
+      return await this.genericGetRequest(endpoint,queryParams)
+    }
+  }
+
+  
+  
